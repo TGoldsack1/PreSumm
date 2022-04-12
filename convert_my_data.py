cf6cffacb1652e013d561adee9b8d1d334aa1790
tgoldsack1 <tgoldsack1@sheffield.ac.uk>
2022-04-12 15:58:21 +0100

    added data conversion files

---
+++ convert_my_data.py
@@ -0,0 +1,31 @@
+'''
+Convert my data into the expectd format:
+.story file, a space between every sentence, summary sentences at the end with @highlight before them
+'''
+
+import json
+import os
+
+data_fps = ["./data/eLife/test.json", "./data/PLOS/test.json"]
+
+for fp in data_fps:
+  with open(fp, "r") as in_file:
+    data = json.loads(in_file.read())
+
+  out_path = "./raw_data/" + fp.split("/")[2]
+  
+  if not os.path.exists(out_path):
+    os.makedirs(out_path)
+
+  for inst_dict in data[:2]:
+    doc = inst_dict['abstract'] +  [item for sublist in inst_dict['sections'] for item in sublist]
+    summ = inst_dict['summary']
+
+
+    with open(f"{out_path}/{inst_dict['id']}.story", "w") as out_file:
+      for line in doc:
+        out_file.write(f"{line}\n\n")
+      
+      for line in summ:
+        out_file.write("@highlight\n\n")
+        out_file.write(f"{line}\n\n")
